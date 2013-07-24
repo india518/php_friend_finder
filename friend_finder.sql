162938ac1c2f7cdd366fffc1c4472c1b045d15cf
India Meisner <india.rae@gmail.com>
2013-07-24 16:50:11 -0700

    re-exported SQL database to clean slate for testing. It works/

--- friend_finder.sql
+++ friend_finder.sql
@@ -1,55 +1,87 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+CREATE DATABASE  IF NOT EXISTS `friend_finder` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `friend_finder`;
+-- MySQL dump 10.13  Distrib 5.5.24, for osx10.5 (i386)
+--
+-- Host: 127.0.0.1    Database: friend_finder
+-- ------------------------------------------------------
+-- Server version	5.5.29
 
-DROP SCHEMA IF EXISTS `friend_finder` ;
-CREATE SCHEMA IF NOT EXISTS `friend_finder` DEFAULT CHARACTER SET latin1 ;
-USE `friend_finder` ;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Table `friend_finder`.`users`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `friend_finder`.`users` ;
+--
+-- Table structure for table `friends`
+--
 
-CREATE  TABLE IF NOT EXISTS `friend_finder`.`users` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `first_name` VARCHAR(100) NOT NULL ,
-  `last_name` VARCHAR(100) NOT NULL ,
-  `email` VARCHAR(100) NOT NULL ,
-  `password` VARCHAR(250) NOT NULL ,
-  `created_at` DATETIME NOT NULL ,
-  `updated_at` DATETIME NOT NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+DROP TABLE IF EXISTS `friends`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `friends` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `friend_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_friends_users_idx` (`user_id`),
+  KEY `fk_friends_users1_idx` (`friend_id`),
+  CONSTRAINT `fk_friends_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_friends_users1` FOREIGN KEY (`friend_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `friends`
+--
 
--- -----------------------------------------------------
--- Table `friend_finder`.`friends`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `friend_finder`.`friends` ;
+LOCK TABLES `friends` WRITE;
+/*!40000 ALTER TABLE `friends` DISABLE KEYS */;
+INSERT INTO `friends` VALUES (1,1,2),(2,2,1);
+/*!40000 ALTER TABLE `friends` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE  TABLE IF NOT EXISTS `friend_finder`.`friends` (
-  `user_id` INT(11) NOT NULL ,
-  `friend_id` INT(11) NOT NULL ,
-  INDEX `fk_friends_users_idx` (`user_id` ASC) ,
-  INDEX `fk_friends_users1_idx` (`friend_id` ASC) ,
-  CONSTRAINT `fk_friends_users`
-    FOREIGN KEY (`user_id` )
-    REFERENCES `friend_finder`.`users` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_friends_users1`
-    FOREIGN KEY (`friend_id` )
-    REFERENCES `friend_finder`.`users` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+--
+-- Table structure for table `users`
+--
 
-USE `friend_finder` ;
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `users`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'India','Meisner','india.rae@gmail.com','ae2b1fca515949e5d54fb22b8ed95575','2013-07-23 21:19:56','0000-00-00 00:00:00'),(2,'Biscuit','Kitty','biscuit@kitty.com','38e79c21fd77f1e13ca31ab8e22db989','2013-07-23 21:20:14','0000-00-00 00:00:00'),(3,'Muffin','Kitty','muffin@kitty.com','57a3ac9602907dc29d12642b10733804','2013-07-23 21:20:33','0000-00-00 00:00:00'),(4,'Maurice','Maurice','maurice@spacecowboy.com','dd099c2f017f990e68f5c90a413abc5d','2013-07-23 21:20:53','0000-00-00 00:00:00'),(5,'Aaron','Zeisler','a@zed.com','95646dc6b6667179c8f653bad93b86c0','2013-07-23 21:45:01','0000-00-00 00:00:00');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-07-24 16:48:43
